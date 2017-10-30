ae1886614a172e116d750e903543cbe082d6e152
mario-serna <31573347+mario-serna@users.noreply.github.com>
2017-10-30 11:36:25 -0600

    Added: Users' routes and functions

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,26 @@
 |
 */
 
+$router->group(['middleware' => []], function () use ($router) {
+    $router->post('/users/login', ['uses' => 'UsersController@getToken']);
+});
+
+$router->group(['middleware' => ['auth']], function () use ($router) {
+
+    //Users
+    $router->get('/users', ['uses' => 'UsersController@getAll']);
+    $router->get('/users/{id}', ['uses' => 'UsersController@getUser']);
+    
+    $router->post('/users', ['uses' => 'UsersController@createUser']);
+
+    $router->put('/users/{id}', ['uses' => 'UsersController@updateUser']);
+    $router->delete('/users/{id}', ['uses' => 'UsersController@deleteUser']);
+});
+
 $router->get('/', function () use ($router) {
     return $router->app->version();
+});
+
+$router->get('/key', function () {
+    return str_random(32);
 });
